330e755a4f88d2f85d2eac216604849212b7a95f
visualstudio <visualstudio@122-PC12>
2019-02-08 18:52:34 -0500

    new node added

--- CSD3354-1-WEEK05/Program.cs
+++ CSD3354-1-WEEK05/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace CSD3354_1_WEEK05
 {
+
+    public class Node
+    {
+        public String floor;
+        public Node next;
+
+        public Node(String fl)
+        {
+            floor = fl;
+            next = null;
+        }
+        public void Print()
+        {
+            Console.Write("|" + floor + "|-->");
+            if (next != null)
+            {
+                next.Print();
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Node myNode = new Node("Ground Floor");
+            myNode.next = new Node("First Floor");
+            myNode.next.next = new Node("Second Floor");
+
+            myNode.Print();
         }
     }
 }
